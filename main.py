17a83c57d7dbbb95a6044e567ce4f2f2b2486fe2
Lyndacrebs <evelynsantos.14asenai@gmail.com>
2026-06-02 13:21:10 -0300

    criando diretório uploads pra guardar os arquivos

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Flask funcionando!"
+def index():
+    return render_template('index.html') #o que essa linhas faz? pra que serve o render_template?
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/upload', methods=['POST']) #pra que serve o route?
+def upload():
+    file = request.files['file'] #pra que serve o request?
+
+    file.save(f'uploads/{file.filename}') #salva o upload dentro de um diretório no projeto
